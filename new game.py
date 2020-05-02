866b2c8ec0853b8daa4cffb1083b505db27f413e
true-bubonchik <alyona.14m@gmail.com>
2020-05-02 12:15:27 +0300

    closed game

--- new game.py
+++ new game.py
@@ -2,7 +2,6 @@ from tkinter import *
 import random
 import time
 
-#12
 
 class Game:
     def __init__(self):
@@ -41,48 +40,48 @@ class Coords:
         self.x2 = x2
         self.y2 = y2
 
-    def within_x(co1, co2):
-        if (co1.x1 > co2.x1 and co1.x1 < co2.x2) \
-        or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
-        or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
-        or (co2.x2 > co1.x1 and co2.x2 < co1.x2):
+def within_x(co1, co2):
+    if (co1.x1 > co2.x1 and co1.x1 < co2.x2) \
+    or (co1.x2 > co2.x1 and co1.x2 < co2.x2) \
+    or (co2.x1 > co1.x1 and co2.x1 < co1.x2) \
+    or (co2.x2 > co1.x1 and co2.x2 < co1.x2):
+        return True
+    else:
+        return False
+
+def within_y(co1, co2):
+    if (co1.y1 > co2.y1 and co1.y1 < co2.y2) \
+    or (co1.y2 > co2.y1 and co1.y2 < co2.y2) \
+    or (co2.y1 > co1.y1 and co2.y1 < co1.y2) \
+    or (co2.y2 > co1.y1 and co2.y2 < co1.y2):
             return True
-        else:
+    else:
             return False
 
-    def within_y(co1, co2):
-        if (co1.y1 > co2.y1 and co1.y1 < co2.y2) \
-        or (co1.y2 > co2.y1 and co1.y2 < co2.y2) \
-        or (co2.y1 > co1.y1 and co2.y1 < co1.y2) \
-        or (co2.y2 > co1.y1 and co2.y2 < co1.y2):
-             return True
-        else:
-             return False
+def collided_left(co1, co2):
+    if within_y(co1, co2):
+        if co1.x1 <= co2.x2 and co1.x1 >= co2.x1:
+            return True
+    return False
 
-    def collided_left(co1, co2):
-        if within_y(co1, co2):
-            if co1.x1 <= co2.x2 and co1.x1 >= co2.x1:
+def collided_right(co1, co2):
+    if within_y(co1, co2):
+        if co1.x2 >= co2.x1 and co1.x2 <= co2.x2:
                 return True
-                return False
+    return False
 
-    def collided_right(co1, co2):
-         if within_y(co1, co2):
-             if co1.x2 >= co2.x1 and co1.x2 <= co2.x2:
-                 return True
-                 return False
+def collided_top(co1, co2):
+    if within_x(co1, co2):
+        if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
+            return True
+    return False
 
-    def collided_top(co1, co2):
-        if within_x(co1, co2):
-            if co1.y1 <= co2.y2 and co1.y1 >= co2.y1:
-                 return True
-                 return False
-
-    def collided_bottom(y, co1, co2):
-        if within_x(co1, co2):
-            y_calc = co1.y2 + y
+def collided_bottom(y, co1, co2):
+    if within_x(co1, co2):
+        y_calc = co1.y2 + y
         if y_calc >= co2.y1 and y_calc <= co2.y2:
             return True
-            return False
+    return False
 
 
 class Sprite:
@@ -104,53 +103,18 @@ class PlatformSprite(Sprite):
             image=self.photo_image, anchor="nw")
         self.coordinates = Coords(x, y, x + width, y + height)
 
-g = Game()
-
-platform1 = PlatformSprite(g, PhotoImage (file="./stickman/platform1.gif"),\
-     0, 480, 100, 10)  
-platform2 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
-    150, 440, 100, 10)
-platform3 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
-    300, 400, 100, 10)
-platform4 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
-    300, 160, 100, 10)
-platform5 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
-    175, 350, 66, 10)
-platform6 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
-    50, 300, 66, 10)
-platform7 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
-    170, 120, 66, 10)
-platform8 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
-    45, 60, 66, 10)
-platform9 = PlatformSprite(g, PhotoImage(file="./stickman/platform3.gif"),\
-    170, 250, 32, 10)
-platform10 = PlatformSprite(g, PhotoImage(file="./stickman/platform3.gif"),\
-    230, 200, 32, 10)
-g.sprites.append(platform1)
-g.sprites.append(platform2)
-g.sprites.append(platform3)
-g.sprites.append(platform4)
-g.sprites.append(platform5)
-g.sprites.append(platform6)
-g.sprites.append(platform7)
-g.sprites.append(platform8)
-g.sprites.append(platform9)
-g.sprites.append(platform10)
-
-g.mainloop()
-
 class StickFigureSprite(Sprite):
     def __init__(self, game):
         Sprite.__init__(self, game)
         self.images_left = [
-            PhotoImage(file="figure-L1.gif"),
-            PhotoImage(file="figure-L2.gif"),
-            PhotoImage(file="figure-L3.gif")
+            PhotoImage(file="./stickman/figure-L2.gif"),
+            PhotoImage(file="./stickman/figure-L1.gif"),
+            PhotoImage(file="./stickman/figure-L3.gif")
         ]
         self.images_right = [
-            PhotoImage(file="figure-R1.gif"),
-            PhotoImage(file="figure-R2.gif"),
-            PhotoImage(file="figure-R3.gif")
+            PhotoImage(file="./stickman/figure-R1.gif"),
+            PhotoImage(file="./stickman/figure-R2.gif"),
+            PhotoImage(file="./stickman/figure-R3.gif")
         ]
         self.image = game.canvas.create_image(200, 470, \
             image=self.images_left[0], anchor="nw")
@@ -193,11 +157,10 @@ class StickFigureSprite(Sprite):
                     image=self.images_left[2])
             else:
                 self.game.canvas.itemconfig(self.image, \
-                    image=self.image_left[self.current_image])
+                    image=self.images_left[self.current_image])
         elif self.x > 0:
             if self.y != 0:
-                self.game.canvas.itemconfig(self.image, \
-                    image=self.images_right[2])
+                self.game.canvas.itemconfig(self.image, image=self.images_right[2])
             else:
                 self.game.canvas.itemconfig(self.image, \
                     image=self.images_right[ \
@@ -217,51 +180,105 @@ class StickFigureSprite(Sprite):
             self.jump_count += 1
             if self.jump_count > 20:
                 self.y = 4
-            if self.y > 0:
-                self.jump_count -= 1
-            co = self.coords()
-            left = True
-            right = True
-            top = True
-            bottom = True
-            falling = True
-            if self.y > 0 and co.y2 >= self.game.canvas_height:
-                self.y = 0
-                bottom = False
-            elif self.y < 0 and co.y1 <= 0:
-                self.y = 0
+        if self.y > 0:
+            self.jump_count -= 1
+        co = self.coords()
+        left = True
+        right = True
+        top = True
+        bottom = True
+        falling = True
+        if self.y > 0 and co.y2 >= self.game.canvas_height:
+            self.y = 0
+            bottom = False
+        elif self.y < 0 and co.y1 <= 0:
+            self.y = 0
+            top = False
+        if self.x > 0 and co.x2 >= self.game.canvas_width:
+            self.x = 0
+            right = False
+        elif self.x < 0 and co.x1 <= 0:
+            self.x = 0
+            left = False
+        for sprite in self.game.sprites:
+            if sprite == self:
+                continue
+            sprite_co = sprite.coords()
+            if top and self.y < 0 and collided_top(co, sprite_co):
+                self.y = -self.y 
                 top = False
-            if self.x > 0 and co.x2 >= self.game.canvas_width:
-                self.x = 0
-                right = False
-            elif self.x < 0 and co.x1 <= 0:
+            if bottom and self.y > 0 and collided_bottom(self.y, co, sprite_co):
+                self.y = sprite_co.y1 - co.y2
+                if self.y < 0:
+                    self.y = 0
+                bottom = False
+                top = False
+            if bottom and falling and self.y == 0 \
+                and co.y2 < self.game.canvas_height \
+                and collided_bottom(1, co, sprite_co):
+                falling = False
+            if left and self.x < 0 and collided_left(co, sprite_co):
                 self.x = 0
                 left = False
-            for sprite in self.game.sprites:
-                if sprite == self:
-                    continue
-                sprite_co = sprite.coords()
-                if top and self.y < 0 and collided_top(co, sprite_co):
-                    self.y = -self.y 
-                    top = False
-                if bottom and self.y > 0 and collided_bottom(self.y \
-                    co, sprite_co):
-                    self.y = sprite_co.y1 - co.y2
-                    if self.y < 0:
-                        self.y = 0
-                    bottom = False
-                    top = False
-                if bottom and falling and self.y == 0 \
-                    and co.y2 < self.game.canvas_height \
-                    and collided_bottom(1, co, sprite_co):
-                    falling = False
+                if sprite.endgame:
+                    self.game.running = False
+            if right and self.x > 0 and collided_right(co, sprite_co):
+                self.x = 0
+                right = False
+                if sprite.endgame:
+                    self.game.running = False
+        if falling and bottom and self.y == 0 \
+            and co.y2 < self.game.canvas_height:
+            self.y = 4
+        self.game.canvas.move(self.image, self.x, self.y)
+
+class DoorSprite(Sprite):
+    def __init__(self, game, photo_image, x, y, width, height):
+        Sprite.__init__(self, game)
+        self.photo_image = photo_image
+        self.image = game.canvas.create_image(x, y, \
+            image=self.photo_image, anchor="nw")
+        self.coordinates = Coords(x, y, x + (width / 2), \
+            y + height)
+        self.endgame = True
 
 g = Game()
-platform1 = PlatformSprite(g, PhotoImage(file="platform1.gif"), \
+platform1 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"), \
     0, 480, 100, 10)
-
+platform2 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
+    150, 440, 100, 10)
+platform3 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
+    300, 400, 100, 10)
+platform4 = PlatformSprite(g, PhotoImage(file="./stickman/platform1.gif"),\
+    300, 160, 100, 10)
+platform5 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
+    175, 350, 66, 10)
+platform6 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
+    50, 300, 66, 10)
+platform7 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
+    170, 120, 66, 10)
+platform8 = PlatformSprite(g, PhotoImage(file="./stickman/platform2.gif"),\
+    45, 60, 66, 10)
+platform9 = PlatformSprite(g, PhotoImage(file="./stickman/platform3.gif"),\
+    170, 250, 32, 10)
+platform10 = PlatformSprite(g, PhotoImage(file="./stickman/platform3.gif"),\
+    230, 200, 32, 10)
 g.sprites.append(platform1)
+g.sprites.append(platform2)
+g.sprites.append(platform3)
+g.sprites.append(platform4)
+g.sprites.append(platform5)
+g.sprites.append(platform6)
+g.sprites.append(platform7)
+g.sprites.append(platform8)
+g.sprites.append(platform9)
+g.sprites.append(platform10)
+door = DoorSprite(g, PhotoImage(file="./stickman/doorl.gif"), 45, 30, 40, 35)
 
-# tk.mainloop()
+g.sprites.append(door)
+sf = StickFigureSprite(g)
+g.sprites.append(sf)
+
+g.mainloop()
 
 # g.tk.mainloop()
